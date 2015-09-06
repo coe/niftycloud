24601c9092879199eab1ef8c190f6d550bab3f22
coe <coe.app@gmail.com>
2015-09-06 19:02:09 +0900

    initial

---
+++ iphone/build/niftycloud.build/Release-iphoneos/niftycloud.build/Objects-normal/armv7/NCMBWrapper.d
@@ -0,0 +1,21 @@
+dependencies: \
+  /Users/user/Documents/Appcelerator_Studio_Workspace/niftycloud/iphone/NCMBWrapper.m \
+  /Users/user/Documents/Appcelerator_Studio_Workspace/niftycloud/iphone/NCMBWrapper.h \
+  /Users/user/Documents/Appcelerator_Studio_Workspace/niftycloud/ncmb_ios/NCMB/NCMB.h \
+  /Users/user/Documents/Appcelerator_Studio_Workspace/niftycloud/ncmb_ios/NCMB/NCMBAnalytics.h \
+  /Users/user/Documents/Appcelerator_Studio_Workspace/niftycloud/ncmb_ios/NCMB/NCMBInstallation.h \
+  /Users/user/Documents/Appcelerator_Studio_Workspace/niftycloud/ncmb_ios/NCMB/NCMBObject.h \
+  /Users/user/Documents/Appcelerator_Studio_Workspace/niftycloud/ncmb_ios/NCMB/NCMBConstants.h \
+  /Users/user/Documents/Appcelerator_Studio_Workspace/niftycloud/ncmb_ios/NCMB/NCMBPush.h \
+  /Users/user/Documents/Appcelerator_Studio_Workspace/niftycloud/ncmb_ios/NCMB/NCMBAnonymousUtils.h \
+  /Users/user/Documents/Appcelerator_Studio_Workspace/niftycloud/ncmb_ios/NCMB/NCMBQuery.h \
+  /Users/user/Documents/Appcelerator_Studio_Workspace/niftycloud/ncmb_ios/NCMB/NCMBGeoPoint.h \
+  /Users/user/Documents/Appcelerator_Studio_Workspace/niftycloud/ncmb_ios/NCMB/NCMBRelation.h \
+  /Users/user/Documents/Appcelerator_Studio_Workspace/niftycloud/ncmb_ios/NCMB/NCMBRole.h \
+  /Users/user/Documents/Appcelerator_Studio_Workspace/niftycloud/ncmb_ios/NCMB/NCMBACL.h \
+  /Users/user/Documents/Appcelerator_Studio_Workspace/niftycloud/ncmb_ios/NCMB/NCMBError.h \
+  /Users/user/Documents/Appcelerator_Studio_Workspace/niftycloud/ncmb_ios/NCMB/NCMBUser.h \
+  /Users/user/Documents/Appcelerator_Studio_Workspace/niftycloud/ncmb_ios/NCMB/NCMBFile.h \
+  /Users/user/Documents/Appcelerator_Studio_Workspace/niftycloud/ncmb_ios/NCMB/NCMBTwitterUtils.h \
+  /Users/user/Documents/Appcelerator_Studio_Workspace/niftycloud/ncmb_ios/NCMB/NCMBSubclassing.h \
+  /Users/user/Documents/Appcelerator_Studio_Workspace/niftycloud/ncmb_ios/NCMB/NCMBReachability/NCMBReachability.h
---
+++ iphone/build/niftycloud.build/Release-iphonesimulator/niftycloud.build/Objects-normal/i386/JpCoeNiftycloudModuleAssets.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/user/Documents/Appcelerator_Studio_Workspace/niftycloud/iphone/Classes/JpCoeNiftycloudModuleAssets.m \
+  /Users/user/Documents/Appcelerator_Studio_Workspace/niftycloud/iphone/Classes/JpCoeNiftycloudModuleAssets.h
